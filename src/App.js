e9a22bbd3713a02515be4197e3c440e9923bdd8e
Ishuwaraa <115783906+Ishuwaraa@users.noreply.github.com>
2024-03-29 01:13:12 +0530

    Components added

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import styled, { ThemeProvider } from 'styled-components';
+import './App.css';
+import { darkTheme } from './utils/themes';
+import Navbar from './components/navbar';
+import HeroSection from './components/heroSection';
+import Skills from './components/skills';
+import Education from './components/education';
+import {BrowserRouter,Routes,Route, useNavigate, Navigate } from 'react-router-dom'
+
+const Body = styled.div`
+  background-color: ${({theme}) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden:
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg, 
+      rgba(204, 0, 187, 0.15) 0%, 
+      rgba(201, 32, 184, 0) 50%
+    ), 
+    linear-gradient(
+      141.27deg, 
+      rgba(0, 70, 209, 0) 50%, 
+      rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Education />
+          </Wrapper>
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/data/constants.js
@@ -0,0 +1,110 @@
+export const Bio = {
+    name: "Ishuwara Avarjana",
+    roles: [
+      "Full Stack Developer",
+      "Mobile app Developer",
+      "UI/UX Designer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/ishuwaraa",
+    resume: "",
+    linkedin: "https://www.linkedin.com/in/ishuwara-avarjana-a83872253/",
+    twitter: "",
+    instagram: "https://www.instagram.com/ishuwara._",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Material UI",
+          image: "",
+        },
+        {
+          name: "Flutter",
+          image:
+            "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Flutter",
+          image:
+            "",
+        },
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "",
+      school: "NSBM",
+      date: "Sep 2022 - May 2025",
+      grade: "",
+      desc: "I am currently pursuing a Bachelor's degree in Software Engineering at NSBM Green University. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web app development and Mobile app development, among others.",
+      degree: "BSc hons in Software Engineering - Plymouth University",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "",
+      date: "Jun 2023 - Jul 2023",
+      description:
+        "",
+      image:
+        "",
+      tags: [
+        "",
+        "",
+      ],
+      github: "https://github.com/ishuwaraa",
+    },   
+  ];
