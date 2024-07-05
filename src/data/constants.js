94f152a8ddbca87f1d5a0be16b19e4ed90779d1d
Ishuwaraa <115783906+Ishuwaraa@users.noreply.github.com>
2024-07-05 21:48:00 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -22,7 +22,7 @@ export const Bio = {
     description:
         "I am a motivated and a versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/ishuwaraa",
-    resume: "https://drive.google.com/file/d/1VhPRizW5VFBp4wbYjzpTY3oAAgIZ3WkI/view?usp=sharing",
+    resume: "https://drive.google.com/drive/folders/1zI9NupEeQk1WfFT3NgimHEoahIaJVDH7?usp=sharing",
     linkedin: "https://www.linkedin.com/in/ishuwara-avarjana-a83872253/",
     instagram: "https://www.instagram.com/ishuwara._",
 };
